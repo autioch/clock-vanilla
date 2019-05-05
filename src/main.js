7b3fa54b169281bc8c26aec1f1dafa0fb8bb16f0
Autioch <autioch@gmail.com>
2019-05-05 13:48:08 +0200

    Review refactor.

--- src/main.js
+++ src/main.js
@@ -1,47 +1,51 @@
-/* View representation. */
-const digitalUi = {
-  hour: document.querySelector('.digital > .hour'),
-  minute: document.querySelector('.digital > .minute'),
-  second: document.querySelector('.digital > .second'),
-  milisecond: document.querySelector('.digital > .milisecond'),
-}
+/* Complete app, kept inside "main" function. */
+function app() {
 
-/* Current state of the app. */
-const currentTime = {
-  hour: 0,
-  minute: 0,
-  second: 0,
-  milisecond: 0
-}
+  /* View representation. */
+  const digitalUi = {
+    hour: document.querySelector('.digital > .hour'),
+    minute: document.querySelector('.digital > .minute'),
+    second: document.querySelector('.digital > .second'),
+    milisecond: document.querySelector('.digital > .milisecond'),
+  }
 
-/* Update state. */
-function getTime(){
-  const now = new Date();
+  /* Get current state */
+  function getCurrentTime() {
+    const now = new Date();
 
-  currentTime.hour = now.getHours();
-  currentTime.minute = now.getMinutes();
-  currentTime.second = now.getSeconds();
-  currentTime.milisecond = now.getMilliseconds();
-}
+    return {
+      hour: now.getHours(),
+      minute: now.getMinutes(),
+      second: now.getSeconds(),
+      milisecond: now.getMilliseconds()
+    };
+  }
 
-/* View utility */
-function formatTime(value){
-  return value < 10 ? `0${value}` : value.toString();
-}
+  /* View utility */
+  function formatTime(value) {
+    return value < 10 ? `0${value}` : value.toString();
+  }
 
-/* Show state in the app. */
-function displayTime(){
-  digitalUi.hour.textContent = formatTime(currentTime.hour);
-  digitalUi.minute.textContent = formatTime(currentTime.minute);
-  digitalUi.second.textContent = formatTime(currentTime.second);
-  digitalUi.milisecond.textContent = formatTime(currentTime.milisecond);
-}
+  /* Show state in the app. */
+  function displayTime(currentTime) {
+    digitalUi.hour.textContent = formatTime(currentTime.hour);
+    digitalUi.minute.textContent = formatTime(currentTime.minute);
+    digitalUi.second.textContent = formatTime(currentTime.second);
+    digitalUi.milisecond.textContent = formatTime(currentTime.milisecond);
+  }
 
-/* Complete app. */
-function app(){
-  getTime();
-  displayTime();
-  setTimeout(app, 10);
+  /* Actual app function. */
+  function run() {
+    const currentTime = getCurrentTime();
+
+    displayTime(currentTime);
+
+    /* Loop the app */
+    setTimeout(run, 10);
+  }
+
+  /* Initial run. */
+  run();
 }
 
 /* Properly wait until whole page is loaded. */
