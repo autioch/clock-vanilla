a0a01203ad1c34267a41b5b6d3b2e36446cd11e8
Autioch <autioch@gmail.com>
2019-05-05 20:49:02 +0200

    =Refactor to extract modes to separate closure

--- src/main.js
+++ src/main.js
@@ -1,15 +1,93 @@
 /* Utilities */
 
-function formatTime(value) {
-  return value < 10 ? `0${value}` : value.toString();
+function toggleVisibility(elToToggle, isVisible){
+  elToToggle.style.display = isVisible ? '' : 'none';
 }
 
-function toggleVisibility(elToToggle, checkboxEl){
-  elToToggle.style.display = checkboxEl.checked ? '' : 'none';
+function digitalMode() {
+
+  const ui = {
+    container: document.querySelector('.digital'),
+    hour: document.querySelector('.digital-display__hour'),
+    minute: document.querySelector('.digital-display__minute'),
+    second: document.querySelector('.digital-display__second'),
+    millisecond: document.querySelector('.digital-display__millisecond'),
+    showMinute: document.querySelector('.digital-options__minute'),
+    showSecond: document.querySelector('.digital-options__second'),
+    showMillisecond: document.querySelector('.digital-options__millisecond'),
+    formatAmPm: document.querySelector('.digital-format__am-pm'),
+    formatFull: document.querySelector('.digital-format__full')
+  };
+
+  const state = {
+    isAmPm: false
+  };
+
+  function toggleFormat() {
+    state.isAmPm = ui.formatAmPm.checked;
+  }
+
+  function formatTime(value) {
+    return value < 10 ? `0${value}` : value.toString();
+  }
+
+  function parseHour(currentHour, isAmPm) {
+    return isAmPm ? currentHour % 12 : currentHour;
+  }
+
+  function bindEvents() {
+    ui.showMinute.addEventListener('change', () => toggleVisibility(ui.minute, ui.showMinute.checked));
+    ui.showSecond.addEventListener('change', () => toggleVisibility(ui.second, ui.showSecond.checked));
+    ui.showMillisecond.addEventListener('change', () => toggleVisibility(ui.millisecond, ui.showMillisecond.checked));
+    ui.formatAmPm.addEventListener('change', toggleFormat);
+    ui.formatFull.addEventListener('change', toggleFormat);
+  }
+
+
+  function render(currentTime) {
+    const parsedHour = parseHour(currentTime.hour, state.isAmPm);
+
+    ui.hour.textContent = formatTime(parsedHour);
+    ui.minute.textContent = formatTime(currentTime.minute);
+    ui.second.textContent = formatTime(currentTime.second);
+    ui.millisecond.textContent = formatTime(currentTime.millisecond);
+  }
+
+  function toggle(isVisible){
+    toggleVisibility(ui.container, isVisible);
+  }
+
+  /* Initial setup. */
+  toggleVisibility(ui.minute, ui.showMinute);
+  toggleVisibility(ui.second, ui.showSecond);
+  toggleVisibility(ui.millisecond, ui.showMillisecond);
+  toggleFormat();
+  bindEvents();
+
+  return {
+    render,
+    toggle
+  };
 }
 
-function parseHour(currentHour, isAmPm) {
-  return isAmPm ? currentHour % 12 : currentHour;
+function clockfaceMode(){
+
+  const ui = {
+    container: document.querySelector('.clockface')
+  };
+
+  function render() {
+
+  }
+
+  function toggle(isVisible){
+    toggleVisibility(ui.container, isVisible);
+  }
+
+  return {
+    render,
+    toggle
+  };
 }
 
 /* Complete app, kept inside "main" function. */
@@ -17,45 +95,21 @@ function app() {
 
   /* View representation. */
   const ui = {
-    mode: {
-      digital: document.querySelector('.mode__digital'),
-      clockface: document.querySelector('.mode__clockface'),
-    },
-    digital: {
-      container: document.querySelector('.digital'),
-      hour: document.querySelector('.digital-display__hour'),
-      minute: document.querySelector('.digital-display__minute'),
-      second: document.querySelector('.digital-display__second'),
-      millisecond: document.querySelector('.digital-display__millisecond'),
-      showMinute: document.querySelector('.digital-options__minute'),
-      showSecond: document.querySelector('.digital-options__second'),
-      showMillisecond: document.querySelector('.digital-options__millisecond'),
-      formatAmPm: document.querySelector('.digital-format__am-pm'),
-      formatFull: document.querySelector('.digital-format__full'),
-    },
-    clockface: {
-      container: document.querySelector('.clockface')
-    }
+    digital: document.querySelector('.mode__digital'),
+    clockface: document.querySelector('.mode__clockface'),
   };
 
+  const digitalClock = digitalMode();
+  const clockfaceClock = clockfaceMode();
+
   const state = {
-    isAmPm: false,
     isDigital: false,
     isClockFace: false
   };
 
   function bindEvents() {
-    ui.mode.digital.addEventListener('change', setMode);
-    ui.mode.clockface.addEventListener('change', setMode);
-    ui.digital.showMinute.addEventListener('change', () => toggleVisibility(ui.digital.minute, ui.digital.showMinute));
-    ui.digital.showSecond.addEventListener('change', () => toggleVisibility(ui.digital.second, ui.digital.showSecond));
-    ui.digital.showMillisecond.addEventListener('change', () => toggleVisibility(ui.digital.millisecond, ui.digital.showMillisecond));
-    ui.digital.formatAmPm.addEventListener('change', toggleFormat);
-    ui.digital.formatFull.addEventListener('change', toggleFormat);
-  }
-
-  function toggleFormat() {
-    state.isAmPm = ui.digital.formatAmPm.checked;
+    ui.digital.addEventListener('change', setMode);
+    ui.clockface.addEventListener('change', setMode);
   }
 
   /* Get current state */
@@ -71,21 +125,11 @@ function app() {
   }
 
   function setMode() {
-    state.isDigital = ui.mode.digital.checked;
-    state.isClockFace = ui.mode.clockface.checked;
+    state.isDigital = ui.digital.checked;
+    state.isClockFace = ui.clockface.checked;
 
-    toggleVisibility(ui.digital.container, ui.mode.digital);
-    toggleVisibility(ui.clockface.container, ui.mode.clockface);
-  }
-
-  /* Show state in the app. */
-  function displayDigitalTime(currentTime) {
-    const parsedHour = parseHour(currentTime.hour, state.isAmPm);
-
-    ui.digital.hour.textContent = formatTime(parsedHour);
-    ui.digital.minute.textContent = formatTime(currentTime.minute);
-    ui.digital.second.textContent = formatTime(currentTime.second);
-    ui.digital.millisecond.textContent = formatTime(currentTime.millisecond);
+    digitalClock.toggle(state.isDigital);
+    clockfaceClock.toggle(state.isClockFace);
   }
 
   /* Actual app function. */
@@ -93,7 +137,11 @@ function app() {
     const currentTime = getCurrentTime();
 
     if (state.isDigital) {
-      displayDigitalTime(currentTime);
+      digitalClock.render(currentTime);
+    }
+
+    if (state.isClockFace) {
+      clockfaceClock.render(currentTime);
     }
 
     /* Loop the app */
@@ -101,10 +149,6 @@ function app() {
   }
 
   /* Initial run. */
-  toggleVisibility(ui.digital.minute, ui.digital.showMinute);
-  toggleVisibility(ui.digital.second, ui.digital.showSecond);
-  toggleVisibility(ui.digital.millisecond, ui.digital.showMillisecond);
-  toggleFormat();
   setMode();
   bindEvents();
   run();
